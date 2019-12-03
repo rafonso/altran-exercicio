bc29d0f82cbabc749d512cc50d6650d885261258
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2019-12-03 19:29:16 -0300

    Criação dos projetos de backend e frontend
    Seguindo o tutorial disponível em https://www.callicoder.com/spring-boot-mongodb-angular-js-rest-api-tutorial/

---
+++ carrinho-compras-backend/src/main/java/rafael/altran/exercicio/carrinhocomprasbackend/CarrinhoComprasBackendApplication.java
@@ -0,0 +1,13 @@
+package rafael.altran.exercicio.carrinhocomprasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoComprasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoComprasBackendApplication.class, args);
+	}
+
+}
---
+++ carrinho-compras-backend/src/test/java/rafael/altran/exercicio/carrinhocomprasbackend/CarrinhoComprasBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package rafael.altran.exercicio.carrinhocomprasbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarrinhoComprasBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
